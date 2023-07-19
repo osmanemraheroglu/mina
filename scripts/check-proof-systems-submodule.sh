9e8642c979767e8d7b5013f607a1b062c11fb98c
dkijania <dariusz@o1labs.org>
2023-07-19 22:30:36 +0200

    add default condition

--- scripts/check-proof-systems-submodule.sh
+++ scripts/check-proof-systems-submodule.sh
@@ -6,15 +6,25 @@ cd src/lib/crypto/proof-systems
 
 CURR=$(git rev-parse HEAD)
 # temporarily skip SSL verification (for CI)
-git config http.sslVerify false
+#git config http.sslVerify false
 git fetch origin
-git config http.sslVerify true
+#git config http.sslVerify true
+
+
+if [ "${BUILDKITE_PULL_REQUEST_BASE_BRANCH}" = "" ]; then
+  echo "BUILDKITE_PULL_REQUEST_BASE_BRANCH env variable is not set. Are you triggered build manually?"
+  echo "If yes, then rerun it using custom build with environment variables:"
+  echo "BUILDKITE_PULL_REQUEST_BASE_BRANCH={your_target_branch}"
+  exit 1
+fi
 
 declare -A BRANCH_MAPPING=(
   ["rampup"]="compatible" 
   ["berkeley"]="berkeley"
   ["develop"]="develop"
   ["izmir"]="master"
+  # Default case
+  [${BUILDKITE_PULL_REQUEST_BASE_BRANCH}]=${BUILDKITE_PULL_REQUEST_BASE_BRANCH}
 )
 
 function in_branch {
