72dd3e0322fda0170a129e23ab4de0868bceebef
dkijania <dariusz@o1labs.org>
2023-07-25 23:17:06 +0200

    increase frequency for graphql polling log engine

--- src/lib/integration_test_cloud_engine/graphql_polling_log_engine.ml
+++ src/lib/integration_test_cloud_engine/graphql_polling_log_engine.ml
@@ -60,7 +60,7 @@ let rec poll_get_filtered_log_entries_node ~logger ~event_writer
     ~last_log_index_seen node =
   let open Deferred.Let_syntax in
   if not (Pipe.is_closed event_writer) then (
-    let%bind () = after (Time.Span.of_ms 10000.0) in
+    let%bind () = after (Time.Span.of_ms 500.0) in
     match%bind
       Integration_test_lib.Graphql_requests.get_filtered_log_entries
         ~last_log_index_seen
