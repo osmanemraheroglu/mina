a222a21e513f9143bc7dc301f7f32659ce33bc34
dkijania <dariusz@o1labs.org>
2023-07-25 23:29:31 +0200

    download postgres 15

--- buildkite/scripts/replayer-test.sh
+++ buildkite/scripts/replayer-test.sh
@@ -12,11 +12,28 @@ export DEBIAN_FRONTEND=noninteractive
 
 # time zone = US Pacific
 /bin/echo -e "12\n10" | apt-get install -y tzdata
-apt-get install -y git postgresql apt-transport-https ca-certificates curl
+apt-get install -y git apt-transport-https ca-certificates curl
 
 git config --global --add safe.directory /workdir
 
 source buildkite/scripts/export-git-env-vars.sh
+
+#download postgres 15
+
+# Create the file repository configuration
+echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" | tee /etc/apt/sources.list.d/pgdg.list
+
+# Import the repository signing key
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo tee /etc/apt/trusted.gpg.d/pgdg.asc &>/dev/null
+
+# Update list of available packages
+sudo apt update
+
+# Download postgres
+sudo apt install postgresql-15 postgresql-client-15 -y
+
+# Check version
+psql --version
 
 echo "deb [trusted=yes] http://packages.o1test.net bullseye ${MINA_DEB_RELEASE}" | tee /etc/apt/sources.list.d/mina.list
 apt-get update
