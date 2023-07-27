b80bb1b183bd470e3eccd8c48a703989efb785af
dkijania <dariusz@o1labs.org>
2023-07-27 09:58:32 +0200

    sudo -u

--- buildkite/scripts/replayer-test.sh
+++ buildkite/scripts/replayer-test.sh
@@ -49,8 +49,8 @@ sudo service postgresql start
 
 echo "Populating archive database"
 cd ~postgres
-su postgres -c psql < $TEST_DIR/archive_db.sql
-echo "ALTER USER postgres PASSWORD '$PGPASSWORD';" | su postgres -c psql
+sudo -u postgres -c psql < $TEST_DIR/archive_db.sql
+echo "ALTER USER postgres PASSWORD '$PGPASSWORD';" | sudo -u postgres -c psql
 cd /workdir
 
 echo "Running replayer"
